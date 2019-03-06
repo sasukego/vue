5307a0ad25322b6f524b52298115fe5b32b0e1e8
mac <mac@macdeMacBook-Pro.local>
2019-03-06 22:30:25 +0800

    abc

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+     <h2>hello</h2>
+   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+   export default {
+     name:'app',
+     data() {
+       return {
+         msg:'welcome to your app'
+       }
+     }
+   }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
